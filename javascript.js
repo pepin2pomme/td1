306b560a61008cb217fdbe0520d76b3c61ccaa59
pepin2pomme <milosorel.7@gmail.com>
2025-09-15 15:13:26 +0200

    feat(js): exercice 2, ajout de rectangle => calcule l'aire

--- javascript.js
+++ javascript.js
@@ -5,3 +5,7 @@ let fahrenheit = celcius * 9/5 + 32
 
 document.body.innerHTML += "Celcius: " + celcius + " = Fahrenheit: " + fahrenheit + "<br>";
 
+let longueur = 10;
+let largeur = 5;
+let aire = longueur*largeur;
+document.body.innerHTML += "Longueur = " + longueur + "; Largeur = " + largeur +"; Aire du rectangle = " + aire;
