6af8dd0aa537d4b6ed3be4ddfc02c2335d45af65
pepin2pomme <milosorel.7@gmail.com>
2025-09-15 15:59:24 +0200

    add(shifumi): ajout de fonctions js, et de la base html du jeu

--- javascript.js
+++ javascript.js
@@ -12,3 +12,28 @@ let aire = longueur*largeur;
 document.body.innerHTML += "Longueur = " + longueur + "; Largeur = " + largeur +"; Aire du rectangle = " + aire;
 */
 
+
+let ptsJoueur =0;
+let ptsOrdi = 0;
+let egalite = 0;
+
+let statutRound = 0;
+
+function miseajour(statutRound){
+
+}
+
+document.getElementById("boutonPierre").addEventListener("click", function () {
+    let ordi = Math.floor(Math.random() * 3);
+    if (ordi == 0) {egalite += 1;} else if (ordi == 1) {ptsOrdi += 1;} else {ptsJoueur += 1;}
+});
+
+document.getElementById("bontonFeuille").addEventListener("click", function () {
+    let ordi = Math.floor(Math.random() * 3);
+    if (ordi == 1) {egalite += 1;} else if (ordi == 2) {ptsOrdi += 1;} else {ptsJoueur += 0;}
+});
+
+document.getElementById("boutonCiseaux").addEventListener("click", function () {
+    let ordi = Math.floor(Math.random() * 3);
+    if (ordi == 2) {egalite += 1;} else if (ordi == 0) {ptsOrdi += 1;} else {ptsJoueur += 1;}
+});
